d125a28cc0081cb600e8ea19d41949a3ad458d95
Casey Link <unnamedrambler@gmail.com>
2009-06-28 10:52:18 -0400

    Move authentication procedure to a background thread.

--- src/com/mp3tunes/android/activity/Login.java
+++ src/com/mp3tunes/android/activity/Login.java
@@ -21,22 +21,30 @@
  ***************************************************************************/
 package com.mp3tunes.android.activity;
 
+import java.util.concurrent.ExecutionException;
+
 import com.binaryelysium.mp3tunes.api.LockerException;
 import com.mp3tunes.android.MP3tunesApplication;
 import com.mp3tunes.android.PrivateAPIKey;
 import com.mp3tunes.android.R;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.view.View.OnClickListener;
 import android.view.View.OnKeyListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * This activity handles authentication.
@@ -49,6 +57,9 @@ public class Login extends Activity
     private EditText mPassField;
     private EditText mUserField;
     private Button mLoginButton;
+    private AlertDialog mProgDialog;
+    
+    private static final int PROGRESS = 0;
 
     String authInfo;
 
@@ -64,13 +75,20 @@ public class Login extends Activity
         System.out.println("user: " + user + " pass: " + pass);
         if ( !user.equals( "" ) && !pass.equals( "" ) )
         {
+            AsyncTask task = new LoginTask().execute( user, pass );
             try
             {
-                doLogin( user, pass );
+                task.get();
             }
-            catch ( LockerException e )
+            catch ( InterruptedException e )
             {
-                // login failed, proceed as normal
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            catch ( ExecutionException e )
+            {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
             }
         }
         setContentView( R.layout.login );
@@ -79,6 +97,18 @@ public class Login extends Activity
         mUserField = ( EditText ) findViewById( R.id.username );
         mLoginButton = ( Button ) findViewById( R.id.sign_in_button );
         mPassField.setOnKeyListener( mKeyListener );
+        
+        AlertDialog.Builder builder;
+        LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+        View layout = inflater.inflate(R.layout.progress_dialog,
+                                       (ViewGroup) findViewById(R.id.layout_root));
+
+        TextView text = (TextView) layout.findViewById(R.id.progress_text);
+        text.setText(R.string.loading_authentication);
+
+        builder = new AlertDialog.Builder(this);
+        builder.setView(layout);
+        mProgDialog = builder.create();
 
         // restore text fields
         if ( icicle != null )
@@ -92,6 +122,18 @@ public class Login extends Activity
                 mPassField.setText( pass );
         }
         mLoginButton.setOnClickListener( mClickListener );
+    }
+    
+    @Override
+    protected Dialog onCreateDialog( int id )
+    {
+        switch ( id )
+        {
+        case PROGRESS:
+            return mProgDialog;
+        default:
+            return null;
+        }
     }
 
     OnClickListener mClickListener = new View.OnClickListener()
@@ -109,28 +151,8 @@ public class Login extends Activity
                         getResources().getString( R.string.ERROR_MISSINGINFO ) );
                 return;
             }
-
-            try
-            {
-                doLogin( user, password );
-
-            }
-            catch ( LockerException e )
-            {
-                if ( e.getMessage().contains( "auth failure" ) )
-                {
-                    MP3tunesApplication.getInstance().presentError( v.getContext(),
-                            getResources().getString( R.string.ERROR_AUTH_TITLE ),
-                            getResources().getString( R.string.ERROR_AUTH ) );
-                    ( ( EditText ) findViewById( R.id.password ) ).setText( "" );
-                }
-                else if ( e.getMessage().contains( "connection issue" ) )
-                {
-                    MP3tunesApplication.getInstance().presentError( v.getContext(),
-                            getResources().getString( R.string.ERROR_SERVER_UNAVAILABLE_TITLE ),
-                            getResources().getString( R.string.ERROR_SERVER_UNAVAILABLE ) );
-                }
-            }
+            showDialog( PROGRESS );
+            new LoginTask().execute( user, password );
         }
     };
 
@@ -149,20 +171,64 @@ public class Login extends Activity
             return false;
         }
     };
-
-    void doLogin( String user, String pass ) throws LockerException
+    
+    private class LoginTask extends AsyncTask<String, Void, String>
     {
-        com.binaryelysium.mp3tunes.api.Locker locker = new com.binaryelysium.mp3tunes.api.Locker(
-                PrivateAPIKey.KEY, user, pass );
-        MP3tunesApplication.getInstance().map.put( "mp3tunes_locker", locker );
-        SharedPreferences settings = getSharedPreferences( PREFS, 0 );
-        SharedPreferences.Editor editor = settings.edit();
-        editor.putString( "mp3tunes_user", user );
-        editor.putString( "mp3tunes_pass", pass );
-        editor.commit();
-        Intent intent = new Intent( Login.this, LockerList.class );
-        startActivity( intent );
-        finish();
+        com.binaryelysium.mp3tunes.api.Locker locker;
+        String user;
+        String pass;
+        @Override
+        protected String doInBackground( String... params )
+        {
+            user = params[0];
+            pass = params[1];
+            try 
+            {
+                locker = new com.binaryelysium.mp3tunes.api.Locker( PrivateAPIKey.KEY, user, pass );
+                return "";
+            } catch (LockerException e) {
+                return e.getMessage();
+            }
+        }
+        
+        @Override
+        protected void onPostExecute( String result )
+        {
+            try
+            {
+                dismissDialog( PROGRESS );    
+            } catch (IllegalArgumentException e) 
+            {
+                // do nothing
+            }
+            
+            if( result.equals( "" )  )
+            {
+                MP3tunesApplication.getInstance().map.put( "mp3tunes_locker", locker );
+                SharedPreferences settings = getSharedPreferences( PREFS, 0 );
+                SharedPreferences.Editor editor = settings.edit();
+                editor.putString( "mp3tunes_user", user );
+                editor.putString( "mp3tunes_pass", pass );
+                editor.commit();
+                Intent intent = new Intent( Login.this, LockerList.class );
+                startActivity( intent );
+                finish();
+            }
+            else if ( result.contains( "auth failure" ) )
+            {
+                MP3tunesApplication.getInstance().presentError( Login.this,
+                        getResources().getString( R.string.ERROR_AUTH_TITLE ),
+                        getResources().getString( R.string.ERROR_AUTH ) );
+                ( ( EditText ) findViewById( R.id.password ) ).setText( "" );
+            }
+            else if ( result.contains( "connection issue" ) )
+            {
+                MP3tunesApplication.getInstance().presentError( Login.this,
+                        getResources().getString( R.string.ERROR_SERVER_UNAVAILABLE_TITLE ),
+                        getResources().getString( R.string.ERROR_SERVER_UNAVAILABLE ) );
+            }
+        }
+        
     }
 
 }
